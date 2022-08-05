8288412884a2c88734f2866c900866900cf6f1c1
Selim Ozcan <65914446+selimozcann@users.noreply.github.com>
2022-08-05 17:02:57 +0300

    Add-LevelManager

---
+++ LevelManager/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private Level[] _level;
+    [SerializeField] private Level testLevel;
+    [SerializeField] private int levelIndex;
+
+    private const string levelStr = "Level";
+    
+    private void Awake()
+    {
+        InitLevel();
+    }
+    private void InitLevel()
+    {
+        levelIndex = GetLevel();
+        if(levelIndex >= 3)
+        {
+            levelIndex = 0;
+        }
+        Level level = testLevel != null ? LevelSpawn(testLevel) : LevelSpawn(_level[levelIndex]);
+    }
+    private Level LevelSpawn(Level level) => Instantiate(level,level.transform.position, Quaternion.identity);
+    private int GetLevel() => PlayerPrefs.GetInt(levelStr);
+    private void SetLevel() => PlayerPrefs.SetInt(levelStr,levelIndex);
+}
